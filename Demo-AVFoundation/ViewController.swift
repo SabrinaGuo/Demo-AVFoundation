1970b3a8506307ef81c32c6a454a367fc93c8aae
sabrinaguo <hsif0111@gmail.com>
2022-07-24 16:55:22 +0800

    程式碼重構：設定各個按鈕變數@IBOutlet，使用 swtich 來判斷點擊哪一個按鈕

--- Demo-AVFoundation/ViewController.swift
+++ Demo-AVFoundation/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     var speechUtterance = AVSpeechUtterance(string:"") // 要說的內容文字
    
     @IBOutlet weak var speakWords: UITextField!// 變數：紀錄 text Field 的內容
+    
+    
+    @IBOutlet weak var btnEnglish: UIButton!
+    
+    @IBOutlet weak var btnMandarin: UIButton!
+    
+    @IBOutlet weak var btnSpanish: UIButton!
+    
+    @IBOutlet weak var btnFrech: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -35,46 +45,33 @@ class ViewController: UIViewController {
         synthesizer.speak(speechUtterance)
     }
 
-    @IBAction func choseEnglish(_ sender: Any) {
-        language = "en-IE"
-       
-        // 如果使用者沒有輸入任何文字時
-        if(speakWords.text==""){
-            speak("please enter text")
+    
+    @IBAction func choseLanguages(_ sender: UIButton) {
+        switch sender{
+        case btnEnglish:
+            language = "en-IE" //語系設定
+            // 如果使用者沒有輸入任何文字時
+            if(speakWords.text==""){
+                speak("please enter text")
+            }
+        case btnMandarin:
+            language = "zh-TW"
+            if(speakWords.text==""){
+                speak("請輸入文字")
+            }
+        case btnSpanish:
+            language = "es-ES"
+            if(speakWords.text==""){
+                speak("por favor ingrese texto")
+            }
+        case btnFrech:
+            language = "fr-FR"
+            if(speakWords.text==""){
+                speak("veuillez entrer du texte")
+            }
+        default: break
         }
-        speak(speakWords.text!)
-    }
-    
-    
-    @IBAction func choseMandarin(_ sender: Any) {
-        language = "zh-TW"
-      
-        // 如果使用者沒有輸入任何文字時
-        if(speakWords.text==""){
-            speak("請輸入文字")
-        }
-        speak(speakWords.text!)
-    }
-    
-    
-    @IBAction func choseSpanish(_ sender: Any) {
-        language = "es-ES"
-       
-        // 如果使用者沒有輸入任何文字時
-        if(speakWords.text==""){
-            speak("por favor ingrese texto")
-        }
-        speak(speakWords.text!)
-    }
-    
-    @IBAction func choseFrech(_ sender: Any) {
-        language = "fr-FR"
-        
-        // 如果使用者沒有輸入任何文字時
-        if(speakWords.text==""){
-            speak("veuillez entrer du texte")
-        }
-        speak(speakWords.text!)
+        speak(speakWords.text!) //唸出來！
     }
     
     //當點擊view任何喔一處鍵盤收起
@@ -85,6 +82,7 @@ class ViewController: UIViewController {
     //點擊鍵盤 return 鍵盤收起
     @IBAction func dismissKeyboard(_ sender: Any) {
         view.endEditing(true)
-    }
+    }   
+
     
 }
